1fd4bdee22ea3e7783cb788b2307cfc8fc8ed49d
Abdus Samad Baksh <abdussamad21360@gmail.com>
2026-07-22 23:22:01 +0545

    Time: 0 ms (100.00%) | Memory: 43 MB (67.38%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -13,17 +13,16 @@ class Solution {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode prev = dummy;
-        while(prev.next != null && prev.next.next != null){
+
+while(prev.next != null && prev.next.next != null){
 ListNode first = prev.next;
 ListNode second = first.next;
-
 first.next = second.next;
 second.next = first;
 prev.next = second;
-
 prev = first;
 
         }
-return dummy.next;
+        return dummy.next;
     }
 }
