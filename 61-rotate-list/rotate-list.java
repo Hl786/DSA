3f12d69b58e727b3126ee9bfbb71697b6329341b
Abdus Samad Baksh <abdussamad21360@gmail.com>
2026-07-26 19:47:00 +0545

    Time: 0 ms (100.00%) | Memory: 44.5 MB (26.24%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -1,42 +1,34 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
 
-        if (head == null || head.next == null || k == 0) {
+        if(head == null || head.next == null)
+            return head;
+
+        ListNode current = head;
+        int count = 1;
+
+        while(current.next != null){
+            count++;
+            current = current.next;
+        }
+
+        k = k % count;
+
+        if(k == 0){
             return head;
         }
 
-        // Find length and tail
-        int length = 1;
-        ListNode tail = head;
+        current.next = head;
 
-        while (tail.next != null) {
-            tail = tail.next;
-            length++;
-        }
+        int steps = count - k;
 
-        // Reduce k
-        k = k % length;
+       for(int i = 0; i < steps; i++){
+    current = current.next;
+}
 
-        if (k == 0) {
-            return head;
-        }
+        head = current.next;
+        current.next = null;
 
-        // Make the list circular
-        tail.next = head;
-
-        // Find the new tail
-        ListNode newTail = head;
-
-        for (int i = 0; i < length - k - 1; i++) {
-            newTail = newTail.next;
-        }
-
-        // New head
-        ListNode newHead = newTail.next;
-
-        // Break the circle
-        newTail.next = null;
-
-        return newHead;
+        return head;
     }
 }
