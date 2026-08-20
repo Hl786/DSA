2ca916044b1f5bc62cfe98bcbe48f185dbbfb07d
Abdus Samad Baksh <abdussamad21360@gmail.com>
2026-08-20 18:13:20 +0545

    Time: 1 ms (77.47%) | Memory: 46.9 MB (9.42%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,49 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-
-        if (nums.length == 0) {
-            return 0;
-        }
-
-        // Create linked list
-        Node head = new Node(nums[0]);
-        Node current = head;
-
-        for (int i = 1; i < nums.length; i++) {
-            current.next = new Node(nums[i]);
-            current = current.next;
-        }
-
-        // Remove duplicates using linked-list logic
-        current = head;
-
-        while (current.next != null) {
-            if (current.val == current.next.val) {
-                current.next = current.next.next;
-            } else {
-                current = current.next;
-            }
-        }
-
-        // Put linked-list result back into array
-        current = head;
-        int index = 0;
-
-        while (current != null) {
-            nums[index] = current.val;
-            index++;
-            current = current.next;
-        }
-
-        return index;
-    }
+        int k = 1;
+        for(int i = 1 ; i < nums.length ; i++ ){
+if(nums[i] != nums[k-1]){
+    nums[k] = nums[i];
+    k++;
 }
-
-class Node {
-    int val;
-    Node next;
-
-    Node(int val) {
-        this.val = val;
+        }
+        return k;
     }
 }
